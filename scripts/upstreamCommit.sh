3b8a7a6742850c0eb23b9e70c95e43c09dfc6ecf
Malfrador <malfrador@gmail.com>
2021-06-24 11:48:32 +0200

    Updated Upstream ()
    Upstream has released updates that appear to apply and compile correctly

--- scripts/upstreamCommit.sh
+++ scripts/upstreamCommit.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+  #!/usr/bin/env bash
 (
 set -e
 PS1="$"
@@ -7,15 +7,15 @@ function changelog() {
     base=$(git ls-tree HEAD $1  | cut -d' ' -f3 | cut -f1)
     cd $1 && git log --oneline ${base}...HEAD
 }
-tuinity=$(changelog Tuinity)
+paper=$(changelog .gradle/caches/paperweight/upstreams/tuinity)
 
 updated=""
 logsuffix=""
-if [ ! -z "$tuinity" ]; then
-    logsuffix="$logsuffix\n\nTuinity Changes:\n$tuinity"
-    if [ -z "$updated" ]; then updated="Tuinity"; else updated="$updated/Tuinity"; fi
+if [ ! -z "$paper" ]; then
+    logsuffix="$logsuffix\n\nTuinity Changes:\n$paper"
+    updated="Tuinity"
 fi
-disclaimer="Upstream has released updates that appears to apply and compile correctly"
+disclaimer="Upstream has released updates that appear to apply and compile correctly"
 
 if [ ! -z "$1" ]; then
     disclaimer="$@"
